c72658134d9654a5653232226d05702d196c7574
Henry Tischler <mysterykeven@gmail.com>
2022-04-11 03:25:51 -0600

    Added functionality to create corpus from folder of writings

--- Corpus.cpp
+++ Corpus.cpp
@@ -1,14 +1,37 @@
 #include "Corpus.h"
 
+Corpus::Corpus(string folderPath){
+    /*This function will construct a corpus from a set of text files in a certain folder. These should be in the format
+    TextName_AuthorName.txt with both names in camel case. Note that if there are two texts with the same author, 
+    they will be combined into one "text" */
+    unordered_map<string, string> authorContent;
+
+    for(auto &file: filesystem::directory_iterator(folderPath)) //Lists all files 
+    {
+        string path = file.path();
+        
+        string splitName = path.substr(path.find_last_of("/")+1, (path.find(".") - path.find_last_of("/")) - 1);
+        string authorName = splitName.substr(splitName.find("_")+1);
+        string content = stringFromFile(file.path());
+
+        if(authorContent.find(authorName) == authorContent.end())
+            authorContent[authorName] = content;
+        else
+            authorContent[authorName] += " " + content; 
+    }
+
+    vector<Text> texts;
+
+    for(pair writing: authorContent){
+        texts.push_back(Text(writing.first, writing.second));
+    }
+
+    this->appendTexts(texts);
+}
+
 Corpus::Corpus(vector<Text> texts){
-    /*Constructs Corpus object. 
-    "Writings" represents a hashmap of writings,
-    based on the array of texts passed. Overall frequencies 
-    represents the word frequencies for the entire corpus*/
-    for(Text t: texts){
-        writings[t.authorName] = t;
-    } 
-    overall_frequencies = sumMaps(this->getFrequenciesVec());
+    /*Constructs Corpus object*/
+    this->appendTexts(texts);
 }
 
 vector<unordered_map<string, double>> Corpus::getFrequenciesVec(){
@@ -21,3 +44,12 @@ vector<unordered_map<string, double>> Corpus::getFrequenciesVec(){
     return frequencies;
 }
 
+void Corpus::appendTexts(vector<Text> texts){
+    /*This should append each text object
+    to the corpus object. Additionally, the 
+    overall frequencies map should be updated.*/
+    for(Text t: texts){
+        writings[t.authorName] = t;
+    } 
+    overall_frequencies = sumMaps(this->getFrequenciesVec());
+}
--- Corpus.h
+++ Corpus.h
@@ -1,11 +1,11 @@
 #pragma once
 
+#include <filesystem>
 #include <vector>
 #include <unordered_map>
+
 #include "Utils.h"
-
 #include "Text.h"
-
 
 using namespace std;
 
@@ -14,6 +14,10 @@ class Corpus{
         unordered_map <string, Text> writings;
         unordered_map <string, double> overall_frequencies; /*This should
         represent the sum of all frequences for every text in writing*/
+
         Corpus(vector<Text> texts);
+        Corpus(string folderPath);
+
         vector<unordered_map<string, double>> getFrequenciesVec();
+        void appendTexts(vector<Text>);
 };
